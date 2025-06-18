f08abe86e6f0579e3fa22d01020381e8a4cc5685
Akash Bindhani <ab791235@gmail.com>
2025-06-18 15:50:31 +0530

    resolve font error

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import './Font/fonts.css'
+import './Font/Fonts.css'
 import { HelmetProvider } from 'react-helmet-async'
 
 
